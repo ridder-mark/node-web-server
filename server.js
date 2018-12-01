873c941cfed962fccaa0e81a6f2a5d2d2dfe7b2a
Rohan Verma <rohanv@BLADE51.redmond.corp.microsoft.com>
2018-12-02 00:45:40 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,55 +1,42 @@
 const express = require('express');
 const hbs = require('hbs');
-const fs = require('fs');
 
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+hbs.registerPartials( __dirname + '/views/partials');
 
-  console.log(log);
-  fs.appendFile('server.log', log + '\n');
-  next();
+// express middleware to set the root directory of HTML pages
+app.use(express.static( __dirname + '/public'));
+
+
+app.use((req,res, next) => {
+    var now = new Date().toString();
+
+    console.log(now);
+    
+    
+    next();
 });
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
-
-app.use(express.static(__dirname + '/public'));
-
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
-});
-
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
-});
 
 app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
+
+// res.send('<h1>welcome to a homepage served by express</h1>');
+
+res.send ( {
+    name: "yolo",
+    likes: "Polo"
+})
 });
 
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
-  });
-});
+app.get('/about', (req, res ) => {
+   res.render('about.hbs', {
+       pageTitle:'About',
+       year:'2018'
+   });
+})
 
-// /bad - send back json with errorMessage
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
-});
+app.listen(3000); 
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
-});
